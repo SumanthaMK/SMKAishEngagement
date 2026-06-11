52418b8a6d07ffa4eda1e239a320a2d019a2b1e7
SumanthaMK <sumanthmuttvalli@gmail.com>
2026-06-12 00:08:42 +0530

    Added Calendar Feature

--- js/script.js
+++ js/script.js
@@ -85,4 +85,77 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCountdown();
     setInterval(updateCountdown, 1000);
 
+    /* =========================
+   ADD TO CALENDAR
+========================= */
+
+const title =
+"Engagement Ceremony - Sumantha & Aishwarya";
+
+const location =
+"SriKrishna Mandira, Tumakuru, Karnataka";
+
+const details =
+"We are delighted to invite you to celebrate our engagement.";
+
+const startDate =
+"20260830T110000";
+
+const endDate =
+"20260830T140000";
+
+/* GOOGLE CALENDAR */
+
+const googleUrl =
+`https://calendar.google.com/calendar/render?action=TEMPLATE
+&text=${encodeURIComponent(title)}
+&dates=${startDate}/${endDate}
+&details=${encodeURIComponent(details)}
+&location=${encodeURIComponent(location)}`;
+
+document
+.getElementById("googleCalendarBtn")
+.href = googleUrl;
+
+/* ICS FILE */
+
+document
+.getElementById("icsBtn")
+.addEventListener("click", () => {
+
+    const icsContent =
+`BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:${title}
+DESCRIPTION:${details}
+LOCATION:${location}
+DTSTART:20260830T110000
+DTEND:20260830T140000
+END:VEVENT
+END:VCALENDAR`;
+
+    const blob =
+        new Blob([icsContent],
+        { type:'text/calendar' });
+
+    const url =
+        window.URL.createObjectURL(blob);
+
+    const link =
+        document.createElement("a");
+
+    link.href = url;
+
+    link.download =
+        "Engagement.ics";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+
+});
+
 });
